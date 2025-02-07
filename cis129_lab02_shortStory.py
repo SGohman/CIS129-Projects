e2a95a2d092a7e58dfc25401e3e390a68804cdd7
SGohman <Sgohman@mail.pima.edu>
2025-02-06 19:41:03 -0700

    Update cis129_lab02_shortStory.py
    Python script for "bacon" short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+#Author: Skyler Gohman
+#My second python lab in my CIS129 Course
+#Prints a simple short story
+
+print("I open my eyes as I wake, the smell of bacon in the air.")
+print("My morning has only started and I start to think of the many tasks in which I must conquer.")
+print('''I then smile and think to myself "One step at a time, first things first...bacon"''')
